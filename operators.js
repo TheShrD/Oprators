bf50420f0672383fa1c651b58661c4728051c65c
TheShrD <shrutideshpande599@gmail.com>
2025-02-15 09:23:58 +0530

    Update operators.js

--- operators.js
+++ operators.js
@@ -4,3 +4,32 @@ let num2 = 3;
 
 // addition
 console.log('num1 + num2 = ', num1 + num2);  // 8
+
+# code 
+console.log(2+3);
+# 
+
+Operator
+Name
+Example
+=
+Assignment operator
+a = 7;        // 7
++=
+Addition assignment
+a += 5;     // a = a + 5
+-=
+Subtraction Assignment
+a -= 2;      // a = a - 2
+*=
+Multiplication Assignment
+a *= 3;      // a = a * 3
+/=
+Division Assignment
+a /= 2;      // a = a / 2
+%=
+Remainder Assignment
+a %= 2;    // a = a % 2
+**=
+Exponentiation Assignment
+a **= 2;    // a = square of a
