f7e0b5ca1eac1eabed3f58c02075eebc536ba502
TheShrD <shrutideshpande599@gmail.com>
2025-02-15 09:25:37 +0530

    Update Arithmetic.js

--- Arithmetic.js
+++ Arithmetic.js
@@ -1,1 +1,30 @@
-
+const number = 3 + 5; // 8
+# When using the + operator in your code, you're basically adding two values together.
+# Below is a list of commonly used arithmetic operators:
+# Operator
+Name
+Example
++
+Addition
+a + b
+-
+Subtraction
+a - b
+*
+Multiplication
+a * b
+/
+Division
+a / b
+%
+Remainder
+a % b
+++
+Increment (increment by 1)
+x++ or ++x
+--
+Decrement (decrement by 1)
+--x or x-- 
+**
+Exponentiation (Power)
+a ** b
