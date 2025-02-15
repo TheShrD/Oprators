18cc89d7d76f3dbda0cdb88960ea34c60af4670b
TheShrD <shrutideshpande599@gmail.com>
2025-02-15 09:38:28 +0530

    Create arithmeticeg.js

---
+++ arithmeticeg.js
@@ -0,0 +1,30 @@
+et num1 = 5;
+let num2 = 3;
+
+// addition
+console.log('num1 + num2 = ', num1 + num2);  // 8
+
+// subtraction
+console.log('num1 - num2 = ', num1 - num2);  // 2
+
+// multiplication
+console.log('num1 * num2 = ', num1 * num2);  // 15
+
+// division
+console.log('num1 / num2 = ', num1 / num2);  // 1.6666666666666667
+
+// remainder
+console.log('num1 % num2 = ', num1 % num2);   // 2
+
+// increment
+console.log('++num1 = ', ++num1); // num1 is now 6
+console.log('num1++ = ', num1++); // prints 6 and then increased to 7
+console.log('num1 = ', num1);     // 7
+
+// decrement
+console.log('--num1 = ', --num1); // num1 is now 6
+console.log('num1-- = ', num1--); // prints 6 and then decreased to 5
+console.log('num1 = ', num1);     // 5
+
+//exponentiation
+console.log('num1 ** num2 =', num1 ** num2);
